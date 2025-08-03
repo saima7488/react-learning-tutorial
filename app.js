aba6ddf8caa3589d69b9e505da145142637eaf3e
saima <saimasiddiqui7488@gmail.com>
2025-08-03 15:40:47 +0530

    EPISODE 2

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, "This is the child div"),
+//   React.createElement("h1", { id: "heading" }, "i am an h1 tag "),
+//   React.createElement("h2", { id: "heading2" }, "i am an h2 tag"),
+//]);
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, "This is the child div"),
-  React.createElement("h1", { id: "heading" }, "i am an h1 tag "),
-  React.createElement("h2", { id: "heading2" }, "i am an h2 tag"),
+  React.createElement(
+    "div",
+    { key: "1", id: "child" },
+    "This is the child div"
+  ),
+  React.createElement("h1", { key: "2", id: "heading" }, "I am an h1  tag"),
+  React.createElement("h2", { key: "3", id: "heading2" }, "I am an h2 tag"),
 ]);
 
 // const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
